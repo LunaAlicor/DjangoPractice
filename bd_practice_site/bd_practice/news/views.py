e3fd4762a0853f462c84b5d2cd68dd8a3cd28d82
LunaAlicor <84154835+LunaAlicor@users.noreply.github.com>
2023-08-23 05:11:21 +0300

    bd_pracitce_site css update
    add hovers

--- bd_practice_site/bd_practice/news/views.py
+++ bd_practice_site/bd_practice/news/views.py
@@ -5,5 +5,5 @@ from .models import Articles
 
 
 def news(request):
-    news = Articles.objects.all()
+    news = Articles.objects.all().order_by('-date')
     return render(request, 'news/news.html', {'news': news})
