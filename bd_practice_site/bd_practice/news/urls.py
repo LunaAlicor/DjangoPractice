7dfed8f42d413590c1068346b86538f1119d9f55
LunaAlicor <84154835+LunaAlicor@users.noreply.github.com>
2023-08-24 00:22:10 +0300

    bd_practice_site update
    1

--- bd_practice_site/bd_practice/news/urls.py
+++ bd_practice_site/bd_practice/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.news, name='news')
+    path('', views.news, name='news'),
+    path('<int:pk>', views.NewsDetail.as_view(), name='news-detail')
 ]
--- bd_practice_site/bd_practice/news/views.py
+++ bd_practice_site/bd_practice/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Articles
+from django.views.generic import DetailView
 
 # Create your views here.
 
@@ -7,3 +8,9 @@ from .models import Articles
 def news(request):
     news = Articles.objects.all().order_by('-date')
     return render(request, 'news/news.html', {'news': news})
+
+
+class NewsDetail(DetailView):
+    model = Articles
+    template_name = 'news/news_details.html'
+    context_object_name = 'article'
