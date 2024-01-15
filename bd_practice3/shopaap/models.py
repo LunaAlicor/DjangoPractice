b6103a1876778a222f0034c40252c95ede8e18ce
LunaAlicor <84154835+LunaAlicor@users.noreply.github.com>
2024-01-15 23:23:24 +0300

    test
    test

--- bd_practice3/shopaap/models.py
+++ bd_practice3/shopaap/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
@@ -16,3 +17,9 @@ class Orders(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product, related_name='orders')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    agreement_accepted = models.BooleanField(default=False)
--- bd_practice3/shopaap/tests.py
+++ bd_practice3/shopaap/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .utils import add_two_numbers
 
-# Create your tests here.
+
+class Add_two_numbersTestCase(TestCase):
+    def test_add_two_numbers(self):
+        result = add_two_numbers(2, 2)
+        self.assertEqual(result, 4)
